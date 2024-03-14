4588c2aded7d4de33c756d77d55bcd2d96da8b20
Benjamin Holzman <bholzman@gmail.com>
2024-03-14 08:45:40 -0400

    Allow CI to publish to ECR (#12)
    * Update Makefile
    
    * Update backend.yml
    
    * add ecr login
    
    * allow CI to publish to ecr
    
    * fixes to policy
    
    * only publish on pushes

--- terraform/shared/main.tf
+++ terraform/shared/main.tf
@@ -1,3 +1,26 @@
+module "github_oidc" {
+  source = "../modules/github_oidc"
+}
+
+resource "aws_iam_role_policy" "auth" {
+  role   = module.github_oidc.role.name
+  policy = <<EOF
+{
+    "Version": "2008-10-17",
+    "Statement": [
+        {
+            "Sid": "AllowGetAuthorizationToken",
+            "Effect": "Allow",
+            "Action": [
+                "ecr:GetAuthorizationToken"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
+
 resource "aws_ecr_repository" "backend" {
   name                 = "backend"
   image_tag_mutability = "MUTABLE"
@@ -9,6 +32,33 @@ resource "aws_ecr_repository" "backend" {
   }
 }
 
+resource "aws_ecr_repository_policy" "backend" {
+  repository = aws_ecr_repository.backend.name
+  policy     = <<EOF
+{
+    "Version": "2008-10-17",
+    "Statement": [
+        {
+            "Sid": "AllowPushPull",
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": "${module.github_oidc.role.arn}"
+            },
+            "Action": [
+                "ecr:GetDownloadUrlForLayer",
+                "ecr:BatchGetImage",
+                "ecr:BatchCheckLayerAvailability",
+                "ecr:PutImage",
+                "ecr:InitiateLayerUpload",
+                "ecr:UploadLayerPart",
+                "ecr:CompleteLayerUpload"
+            ]
+        }
+    ]
+}
+EOF
+}
+
 resource "aws_ecr_repository" "anvil" {
   name                 = "anvil"
   image_tag_mutability = "MUTABLE"
@@ -18,6 +68,33 @@ resource "aws_ecr_repository" "anvil" {
   encryption_configuration {
     encryption_type = "AES256"
   }
+}
+
+resource "aws_ecr_repository_policy" "anvil" {
+  repository = aws_ecr_repository.anvil.name
+  policy     = <<EOF
+{
+    "Version": "2008-10-17",
+    "Statement": [
+        {
+            "Sid": "AllowPushPull",
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": "${module.github_oidc.role.arn}"
+            },
+            "Action": [
+                "ecr:GetDownloadUrlForLayer",
+                "ecr:BatchGetImage",
+                "ecr:BatchCheckLayerAvailability",
+                "ecr:PutImage",
+                "ecr:InitiateLayerUpload",
+                "ecr:UploadLayerPart",
+                "ecr:CompleteLayerUpload"
+            ]
+        }
+    ]
+}
+EOF
 }
 
 moved {
