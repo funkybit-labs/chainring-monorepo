4fde059ff72638df79791c8627cd83e234c82adf
Brendan Flood <162615268+bflood366@users.noreply.github.com>
2024-03-18 10:04:50 -0400

    speed up contract deployment, EIP1559 gas settings amd web3j logging (#20)
    * speed up contract deployment, EIP1559 gas settings amd web3j logging
    
    * MR feedback

---
+++ backend/src/main/kotlin/co/chainring/core/blockchain/GasProvider.kt
@@ -0,0 +1,45 @@
+package co.chainring.core.blockchain
+
+import io.github.oshai.kotlinlogging.KotlinLogging
+import org.web3j.protocol.Web3j
+import org.web3j.protocol.core.DefaultBlockParameterName
+import org.web3j.tx.Contract
+import org.web3j.tx.gas.ContractEIP1559GasProvider
+import java.math.BigInteger
+
+open class GasProvider(
+    private val contractCreationLimit: BigInteger,
+    private val contractInvocationLimit: BigInteger,
+    private val defaultMaxPriorityFeePerGas: BigInteger,
+    private val chainId: Long,
+    val web3j: Web3j,
+) : ContractEIP1559GasProvider {
+
+    val logger = KotlinLogging.logger {}
+    override fun getGasLimit(contractFunc: String?): BigInteger {
+        return when (contractFunc) {
+            Contract.FUNC_DEPLOY -> contractCreationLimit
+            else -> contractInvocationLimit
+        }
+    }
+    override fun getGasPrice(contractFunc: String?): BigInteger {
+        return BigInteger.ZERO
+    }
+    override fun isEIP1559Enabled() = true
+    override fun getChainId() = chainId
+
+    override fun getMaxFeePerGas(contractFunc: String?): BigInteger {
+        return getMaxFeePerGas(getMaxPriorityFeePerGas(""))
+    }
+
+    private fun getMaxFeePerGas(maxPriorityFeePerGas: BigInteger): BigInteger {
+        val baseFeePerGas = web3j.ethGetBlockByNumber(DefaultBlockParameterName.PENDING, false).send().block.baseFeePerGas
+        return baseFeePerGas.multiply(BigInteger.TWO).add(maxPriorityFeePerGas)
+    }
+
+    // TODO : In the past we perioidically retrieved this value from alchemy and persisted it
+    override fun getMaxPriorityFeePerGas(contractFunc: String?): BigInteger = defaultMaxPriorityFeePerGas
+
+    override fun getGasLimit(): BigInteger = contractCreationLimit
+    override fun getGasPrice(): BigInteger = getGasPrice("")
+}
