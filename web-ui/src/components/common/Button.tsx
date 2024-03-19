9c5fbc336cd8968031f65ba2c17db34ddc71b630
Benjamin Holzman <bholzman@gmail.com>
2024-03-19 12:42:10 -0400

    PR feedback

--- web-ui/src/components/common/Button.tsx
+++ web-ui/src/components/common/Button.tsx
@@ -4,7 +4,7 @@ type Props = {
   caption: () => string,
   onClick: () => void
 }
-export const Button: FunctionComponent<Props> = ({caption, onClick}) => {
+export function Button({caption, onClick}: Props) {
   return <button className="p-2 bg-darkGray rounded-lg border-transparent px-4 py-2 text-sm font-medium text-white focus:outline-none focus:ring-1 focus:ring-inset"
                  onClick={onClick}>
     {caption()}
