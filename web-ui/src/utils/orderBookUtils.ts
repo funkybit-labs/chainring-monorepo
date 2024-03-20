1805d70db42b0a40f5f896113e27bcf5ac3625bc
Benjamin Holzman <bholzman@gmail.com>
2024-03-20 15:04:12 -0400

    lint

--- web-ui/src/utils/orderBookUtils.ts
+++ web-ui/src/utils/orderBookUtils.ts
@@ -6,9 +6,9 @@ export function calculateTickSpacing(
   // we want to pick "nice" tick numbers which are round-ish numbers but which mostly fill the available space
   // allowed ticks are of the form 10^x * s for some x with s in {1, 2.5, 5, 7.5}
   const delta = maxValue - minValue
-  let rawTickSize = delta / numberOfTicks
-  let rawTickX = Math.floor(Math.log10(rawTickSize))
-  let rawTickS = rawTickSize / Math.pow(10, rawTickX)
+  const rawTickSize = delta / numberOfTicks
+  const rawTickX = Math.floor(Math.log10(rawTickSize))
+  const rawTickS = rawTickSize / Math.pow(10, rawTickX)
   let roundedTickS: number
   let roundedTickX = rawTickX
   // clamp to the nearest allowed value
