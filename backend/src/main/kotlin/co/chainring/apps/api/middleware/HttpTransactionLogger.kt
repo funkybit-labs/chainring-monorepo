80a89d656a1c66158374e1229a39c42b8b8d321b
imykolenko <imykolenko@chainring.co>
2024-03-27 13:47:18 +0100

    log in json, trace id via MDC

--- backend/src/main/kotlin/co/chainring/apps/api/middleware/HttpTransactionLogger.kt
+++ backend/src/main/kotlin/co/chainring/apps/api/middleware/HttpTransactionLogger.kt
@@ -1,40 +1,63 @@
 package co.chainring.apps.api.middleware
 
 import io.github.oshai.kotlinlogging.KLogger
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.JsonNull
 import org.http4k.core.Filter
 import org.http4k.core.HttpTransaction
 import org.http4k.filter.ResponseFilters
 
+@Serializable
+data class HttpRequestLog(
+    val durationMs: Long,
+    val message: String,
+    val req: RequestDetails,
+    val res: ResponseDetails,
+)
+
+@Serializable
+data class RequestDetails(
+    val method: String,
+    val uri: String,
+    val headers: Map<String, String?>,
+    val body: JsonElement,
+)
+
+@Serializable
+data class ResponseDetails(
+    val status: String,
+    val headers: Map<String, String?>,
+    val body: JsonElement,
+)
+
 object HttpTransactionLogger {
+
     operator fun invoke(logger: KLogger): Filter =
         ResponseFilters.ReportHttpTransaction { tx: HttpTransaction ->
-            logger.info {
-                "${tx.request.method.name} ${tx.request.uri} returned ${tx.response.status} in ${tx.duration.toMillis()}ms. ${
-                    headerInfo(
-                        tx,
-                        "X-Forwarded-For",
-                        "from",
-                    )
-                }"
-            }
-            try {
-                logger.info {
-                    """Request headers: ${
-                        tx.request.headers.joinToString("\n ") { "${it.first}: ${it.second}" }
-                    }\nRequest body: '${
-                        tx.request.bodyString()
-                    }'\nResponse body: '${
-                        tx.response.bodyString()
-                    }'"""
-                }
-            } catch (e: Exception) {
-                logger.error(e) { "could not log request and response" }
-            }
-        }
+            val log = HttpRequestLog(
+                message = "${tx.request.method.name} ${tx.request.uri} returned ${tx.response.status} in ${tx.duration.toMillis()}ms.",
+                durationMs = tx.duration.toMillis(),
+                req = RequestDetails(
+                    method = tx.request.method.name,
+                    uri = tx.request.uri.toString(),
+                    headers = tx.request.headers.toMap(),
+                    body = when (val body = tx.request.bodyString()) {
+                        "" -> JsonNull
+                        else -> Json.parseToJsonElement(body)
+                    },
+                ),
+                res = ResponseDetails(
+                    status = tx.response.status.toString(),
+                    headers = tx.response.headers.toMap(),
+                    body = when (val body = tx.response.bodyString()) {
+                        "" -> JsonNull
+                        else -> Json.parseToJsonElement(body)
+                    },
+                ),
+            )
 
-    private fun headerInfo(
-        tx: HttpTransaction,
-        headerName: String,
-        label: String,
-    ) = tx.request.header(headerName)?.let { " $label $it" } ?: ""
+            logger.info { Json.encodeToString(HttpRequestLog.serializer(), log) }
+        }
 }
