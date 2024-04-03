a57e02a8de4bb5312cf9ba79ed59749e22d42196
Benjamin Holzman <bholzman@gmail.com>
2024-04-03 09:11:41 -0400

    CHAIN-73: change sequencer unit tests to bypass chronicle queue

--- sequencer/src/main/kotlin/co/chainring/sequencer/apps/SequencerApp.kt
+++ sequencer/src/main/kotlin/co/chainring/sequencer/apps/SequencerApp.kt
@@ -8,6 +8,7 @@ import co.chainring.sequencer.core.toMarketId
 import co.chainring.sequencer.proto.OrderChanged
 import co.chainring.sequencer.proto.SequencerError
 import co.chainring.sequencer.proto.SequencerRequest
+import co.chainring.sequencer.proto.SequencerResponse
 import co.chainring.sequencer.proto.TradeCreated
 import co.chainring.sequencer.proto.marketCreated
 import co.chainring.sequencer.proto.sequencerResponse
@@ -20,6 +21,71 @@ class SequencerApp : BaseApp() {
     private lateinit var sequencerThread: Thread
     private var markets = mutableMapOf<String, Market>()
 
+    fun processRequest(request: SequencerRequest, sequence: Long = 0L, startTime: Long = 0L): SequencerResponse {
+        return when (request.type) {
+            SequencerRequest.Type.AddMarket -> {
+                val market = request.addMarket!!
+                var error: SequencerError? = null
+                if (markets.containsKey(market.marketId)) {
+                    error = SequencerError.MarketExists
+                } else {
+                    markets[market.marketId] = Market(
+                        market.marketId.toMarketId(),
+                        market.tickSize.toBigDecimal(),
+                        market.marketPrice.toBigDecimal(),
+                        market.maxLevels,
+                        market.maxOrdersPerLevel,
+                    )
+                }
+                sequencerResponse {
+                    this.guid = market.guid
+                    this.sequence = sequence
+                    this.processingTime = System.nanoTime() - startTime
+                    error?.let { this.error = it } ?: run {
+                        this.marketsCreated.add(
+                            marketCreated {
+                                this.marketId = market.marketId
+                                this.tickSize = market.tickSize
+                            },
+                        )
+                    }
+                }
+            }
+            SequencerRequest.Type.ApplyOrderBatch -> {
+                var ordersChanged: List<OrderChanged> = emptyList()
+                var trades: List<TradeCreated> = emptyList()
+                val orderBatch = request.orderBatch!!
+                var error: SequencerError? = null
+                if (markets.containsKey(orderBatch.marketId)) {
+                    val result = markets[orderBatch.marketId]!!.addOrders(orderBatch.ordersToAddList)
+                    ordersChanged = result.ordersChanged
+                    trades = result.createdTrades
+                } else {
+                    error = SequencerError.UnknownMarket
+                }
+                sequencerResponse {
+                    this.sequence = sequence
+                    this.processingTime = System.nanoTime() - startTime
+                    this.guid = orderBatch.guid
+                    this.ordersChanged.addAll(ordersChanged)
+                    this.tradesCreated.addAll(trades)
+                    error?.let {
+                        this.error = it
+                    }
+                }
+            }
+
+            null, SequencerRequest.Type.UNRECOGNIZED -> {
+                sequencerResponse {
+                    this.sequence = sequence
+                    this.processingTime = System.nanoTime() - startTime
+                    this.guid = request.guid
+                    this.error = SequencerError.UnknownRequest
+                }
+            }
+        }
+    }
+
     override fun start() {
         logger.info { "Starting Sequencer App" }
         sequencerThread = thread(start = true, name = "sequencer", isDaemon = false) {
@@ -31,71 +97,9 @@ class SequencerApp : BaseApp() {
                         val startTime = System.nanoTime()
                         dc.wire()?.read()?.bytes { bytes ->
                             val request = SequencerRequest.parseFrom(bytes.toByteArray())
-                            val response = when (request.type) {
-                                SequencerRequest.Type.AddMarket -> {
-                                    val market = request.addMarket!!
-                                    var error: SequencerError? = null
-                                    if (markets.containsKey(market.marketId)) {
-                                        error = SequencerError.MarketExists
-                                    } else {
-                                        markets[market.marketId] = Market(
-                                            market.marketId.toMarketId(),
-                                            market.tickSize.toBigDecimal(),
-                                            market.marketPrice.toBigDecimal(),
-                                            market.maxLevels,
-                                            market.maxOrdersPerLevel,
-                                        )
-                                    }
-                                    sequencerResponse {
-                                        this.guid = market.guid
-                                        this.sequence = dc.index()
-                                        this.processingTime = System.nanoTime() - startTime
-                                        error?.let { this.error = it } ?: run {
-                                            this.marketsCreated.add(
-                                                marketCreated {
-                                                    this.marketId = market.marketId
-                                                    this.tickSize = market.tickSize
-                                                },
-                                            )
-                                        }
-                                    }
-                                }
-                                SequencerRequest.Type.ApplyOrderBatch -> {
-                                    var ordersChanged: List<OrderChanged> = emptyList()
-                                    var trades: List<TradeCreated> = emptyList()
-                                    val orderBatch = request.orderBatch!!
-                                    var error: SequencerError? = null
-                                    if (markets.containsKey(orderBatch.marketId)) {
-                                        val result = markets[orderBatch.marketId]!!.addOrders(orderBatch.ordersToAddList)
-                                        ordersChanged = result.ordersChanged
-                                        trades = result.createdTrades
-                                    } else {
-                                        error = SequencerError.UnknownMarket
-                                    }
-                                    sequencerResponse {
-                                        this.sequence = dc.index()
-                                        this.processingTime = System.nanoTime() - startTime
-                                        this.guid = orderBatch.guid
-                                        this.ordersChanged.addAll(ordersChanged)
-                                        this.tradesCreated.addAll(trades)
-                                        error?.let {
-                                            this.error = it
-                                        }
-                                    }
-                                }
-
-                                null, SequencerRequest.Type.UNRECOGNIZED -> {
-                                    sequencerResponse {
-                                        this.sequence = dc.index()
-                                        this.processingTime = System.nanoTime() - startTime
-                                        this.guid = request.guid
-                                        this.error = SequencerError.UnknownRequest
-                                    }
-                                }
-                            }
                             outputAppender.writingDocument().use {
                                 it.wire()?.write()?.bytes(
-                                    response.toByteArray(),
+                                    processRequest(request, dc.index(), startTime).toByteArray(),
                                 )
                             }
                         }
