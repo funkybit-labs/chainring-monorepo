7c1c9cea5de3926c071f6203f43050a191abbbc4
Anton Onyshchenko <aonyshchenko@chainring.co>
2024-04-11 17:57:26 +0200

    Rename queues so that sequencer starts from scratch in test env

--- sequencer/src/main/kotlin/co/chainring/sequencer/core/Queues.kt
+++ sequencer/src/main/kotlin/co/chainring/sequencer/core/Queues.kt
@@ -5,6 +5,6 @@ import net.openhft.chronicle.queue.ChronicleQueue
 
 val queueHome: String = System.getenv("QUEUE_HOME") ?: OS.getTarget()
 
-val inputQueue = ChronicleQueue.singleBuilder("$queueHome/input").build()
-val outputQueue = ChronicleQueue.singleBuilder("$queueHome/output").build()
-val sequencedQueue = ChronicleQueue.singleBuilder("$queueHome/sequenced").build()
+val inputQueue = ChronicleQueue.singleBuilder("$queueHome/input_queue").build()
+val outputQueue = ChronicleQueue.singleBuilder("$queueHome/output_queue").build()
+val sequencedQueue = ChronicleQueue.singleBuilder("$queueHome/sequenced_queue").build()
