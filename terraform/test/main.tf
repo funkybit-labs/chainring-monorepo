8d33863d7bb0404bca6bd75274c112a8cb33b121
imykolenko <imykolenko@chainring.co>
2024-04-17 12:45:32 +0200

    CHAIN-111 run otterscan block explorer in test

--- terraform/test/main.tf
+++ terraform/test/main.tf
@@ -48,6 +48,7 @@ module "anvil" {
   name_prefix           = local.name_prefix
   task_name             = "anvil"
   image                 = "anvil"
+  memory                = 4096
   ecs_cluster_id        = module.ecs.cluster.id
   app_ecs_task_role     = module.ecs.app_ecs_task_role
   aws_region            = var.aws_region
@@ -64,6 +65,29 @@ module "anvil" {
   health_check          = "/"
   health_check_status   = "400"
   mount_efs_volume      = true
+  service_discovery_private_dns_namespace = module.vpc.service_discovery_private_dns_namespace
+}
+
+module "otterscan" {
+  source                = "../modules/ecs_task"
+  name_prefix           = local.name_prefix
+  task_name             = "otterscan"
+  image                 = "otterscan"
+  ecs_cluster_id        = module.ecs.cluster.id
+  app_ecs_task_role     = module.ecs.app_ecs_task_role
+  aws_region            = var.aws_region
+  subnet_id_1           = module.vpc.private_subnet_id_1
+  subnet_id_2           = module.vpc.private_subnet_id_2
+  vpc                   = module.vpc.vpc
+  allow_inbound         = true
+  hostnames             = ["${local.name_prefix}-otterscan.${data.terraform_remote_state.shared.outputs.zone.name}"]
+  lb_https_listener_arn = module.alb.https_listener_arn
+  lb_priority           = 102
+  lb_dns_name           = module.alb.dns_name
+  zone                  = data.terraform_remote_state.shared.outputs.zone
+  tcp_ports             = [80]
+  health_check          = "/"
+  health_check_status   = "200"
   service_discovery_private_dns_namespace = module.vpc.service_discovery_private_dns_namespace
 }
 
