13ce0b857871b16713d6164c2aa1e5c580d38a11
Scott Burke <sburke@txkdata.com>
2024-04-26 12:04:35 -0500

    added SSL verification CNAME for chainringlabs.com

--- terraform/shared/outputs.tf
+++ terraform/shared/outputs.tf
@@ -6,6 +6,10 @@ output "finance_zone" {
   value = aws_route53_zone.zone-finance
 }
 
+output "labs_zone" {
+  value = aws_route53_zone.zone-labs
+}
+
 output "ci_role_arn" {
   value = module.github_oidc.role.arn
 }
