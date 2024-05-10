5c329e63da25fd4d07e81264a48c9da2d72edf98
Benjamin Holzman <bholzman@gmail.com>
2024-05-10 13:35:22 -0400

    CHAIN-178: update order book to match design

--- web-ui/src/components/common/Widget.tsx
+++ web-ui/src/components/common/Widget.tsx
@@ -3,11 +3,16 @@ import { classNames } from 'utils'
 type Props = {
   contents: JSX.Element
   span?: number
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  wrapperRef?: any
 }
 
-export function Widget({ contents, span }: Props) {
+export function Widget({ contents, span, wrapperRef }: Props) {
   return (
-    <div className={classNames(span ? `col-span-${span}` : '')}>
+    <div
+      ref={wrapperRef}
+      className={classNames(span ? `col-span-${span}` : '')}
+    >
       <div className="rounded-lg bg-darkBluishGray9 p-4 text-white shadow-lg">
         {contents}
       </div>
