3270722e3b4bfb269d0f9072b1a1f1ea39278ee7
imykolenko <imykolenko@chainring.co>
2024-05-23 16:22:35 +0200

    debug logging on deposit

--- mocker/src/main/kotlin/co/chainring/mocker/core/Actor.kt
+++ mocker/src/main/kotlin/co/chainring/mocker/core/Actor.kt
@@ -5,6 +5,7 @@ import co.chainring.core.client.rest.ApiClient
 import co.chainring.integrationtests.utils.Faucet
 import co.chainring.core.utils.TraceRecorder
 import co.chainring.integrationtests.utils.Wallet
+import io.github.oshai.kotlinlogging.KotlinLogging
 import org.web3j.crypto.Keys
 import java.math.BigInteger
 import java.util.UUID
@@ -17,27 +18,38 @@ open class Actor(val native: BigInteger?, val assets: Map<String, BigInteger>, k
     protected var balances = mutableMapOf<String, BigInteger>()
     protected var pendingTrades = mutableListOf<Trade>()
     protected var settledTrades = mutableListOf<Trade>()
+    val log = KotlinLogging.logger {}
 
     protected fun depositAssets() {
         // quick fix for org.web3j.protocol.exceptions.JsonRpcError: replacement transaction underpriced
         // looks like same nonce is re-used due to concurrent start of actors which lead to exiting thread
-        synchronized(Actor::class) {
-            Faucet.fund(wallet.address, (native ?: 2.toFundamentalUnits(18)) * BigInteger.TWO)
-            native?.let { wallet.depositNative(it) }
-            assets.forEach { (symbol, amount) ->
-                wallet.mintERC20(symbol, amount)
-                wallet.depositERC20(symbol, amount)
-            }
-            val fundedAssets = mutableSetOf<String>()
-            while (fundedAssets.size < assets.size + if (native == null) 0 else 1) {
-                Thread.sleep(100)
-                apiClient.getBalances().balances.forEach {
-                    if (it.available > BigInteger.ZERO) {
-                        fundedAssets.add(it.symbol.value)
+        var deposited = false
+        log.debug { "Going to deposit actor $id" }
+        while (!deposited) {
+            try {
+                synchronized(Actor::class) {
+                    Faucet.fund(wallet.address, (native ?: 2.toFundamentalUnits(18)) * BigInteger.TWO)
+                    native?.let { wallet.depositNative(it) }
+                    assets.forEach { (symbol, amount) ->
+                        wallet.mintERC20(symbol, amount)
+                        wallet.depositERC20(symbol, amount)
                     }
-                    balances[it.symbol.value] = it.available
+                    val fundedAssets = mutableSetOf<String>()
+                    while (fundedAssets.size < assets.size + if (native == null) 0 else 1) {
+                        Thread.sleep(100)
+                        apiClient.getBalances().balances.forEach {
+                            if (it.available > BigInteger.ZERO) {
+                                fundedAssets.add(it.symbol.value)
+                            }
+                            balances[it.symbol.value] = it.available
+                        }
+                    }
                 }
+                deposited = true
+            } catch (e: Exception) {
+                log.warn(e) { "Retrying deposit for actor $id because of ${e.message}" }
             }
         }
+        log.debug { "Deposited actor $id" }
     }
 }
