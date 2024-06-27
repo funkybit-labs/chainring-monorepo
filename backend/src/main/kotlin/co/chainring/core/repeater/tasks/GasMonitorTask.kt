86e50a2d3d0e5641fb40c49c072691402e136cff
Benjamin Holzman <bholzman@gmail.com>
2024-06-27 09:57:32 -0400

    CHAIN-328: gas monitoring on submitter (#326)

---
+++ backend/src/main/kotlin/co/chainring/core/repeater/tasks/GasMonitorTask.kt
@@ -0,0 +1,33 @@
+package co.chainring.core.repeater.tasks
+
+import co.chainring.core.blockchain.ChainManager
+import co.chainring.core.model.db.ChainId
+import io.github.oshai.kotlinlogging.KotlinLogging
+import java.math.BigInteger
+import kotlin.time.Duration.Companion.seconds
+
+val warned = mutableSetOf<ChainId>()
+
+class GasMonitorTask(private val minimumGasUnits: BigInteger? = null) : RepeaterBaseTask(
+    invokePeriod = 30.seconds,
+) {
+    val logger = KotlinLogging.logger {}
+    override fun runWithLock() {
+        ChainManager.getBlockchainClients().forEach {
+            // set minimum amount to be equivalent to minimum gas units (or 100,000,000 if not specified) at the max
+            // priority fee, as this scales well with the actual gas needed
+            val minimumGas = (minimumGasUnits ?: BigInteger.valueOf(100_000_000L)) * it.gasProvider.getMaxPriorityFeePerGas(null)
+            val availableGas = it.getNativeBalance(it.submitterAddress)
+            if (availableGas < minimumGas) {
+                if (!warned.contains(it.chainId)) {
+                    logger.error {
+                        "Low gas alert for submitter ${it.submitterAddress} on ${it.chainId}: only $availableGas available, need a minimum of $minimumGas"
+                    }
+                    warned.add(it.chainId)
+                }
+            } else {
+                warned.remove(it.chainId)
+            }
+        }
+    }
+}
---
+++ backend/src/main/kotlin/co/chainring/core/repeater/tasks/RepeaterBaseTask.kt
@@ -0,0 +1,62 @@
+package co.chainring.core.repeater.tasks
+
+import io.github.oshai.kotlinlogging.KotlinLogging
+import kotlinx.datetime.Clock
+import java.util.TimerTask
+import java.util.concurrent.ScheduledThreadPoolExecutor
+import java.util.concurrent.Semaphore
+import java.util.concurrent.TimeUnit
+import kotlin.time.Duration
+
+abstract class RepeaterBaseTask(
+    val invokePeriod: Duration,
+    val initialDelay: Duration = Duration.ZERO,
+    val maxPlannedExecutionTime: Duration? = null,
+) : TimerTask() {
+
+    private val logger = KotlinLogging.logger {}
+
+    private val lock = Semaphore(1, true)
+
+    // keeping `run` final to make sure all exceptions are logged centrally
+    final override fun run() {
+        val isLockAcquired = lock.tryAcquire()
+        if (isLockAcquired) {
+            val startTime = Clock.System.now()
+            try {
+                logger.debug { "Acquired lock for ${this.javaClass.simpleName}" }
+                runWithLock()
+            } catch (t: Throwable) {
+                logger.error(t) { "Error running ${this.javaClass.simpleName}" }
+            } finally {
+                maxPlannedExecutionTime?.let { maxExecutionTime ->
+                    val executionTime = Clock.System.now() - startTime
+                    if (executionTime > maxExecutionTime) {
+                        logger.error { "The execution time of ${this.javaClass.simpleName} ($executionTime) has exceeded the maximum planned time ($maxExecutionTime). Please consider optimizing it." }
+                    }
+                }
+                try {
+                    lock.release()
+                    logger.debug { "Lock released for ${this.javaClass.simpleName}" }
+                } catch (t: Throwable) {
+                    logger.warn(t) { "Could not release lock, retrying" }
+                    try {
+                        lock.release()
+                    } catch (t: Throwable) {
+                        logger.error(t) { "Still could not release lock, giving up" }
+                    }
+                }
+            }
+        } else {
+            logger.warn { "Could not acquire lock for ${this.javaClass.simpleName}, skipping" }
+        }
+    }
+
+    open fun runWithLock() {}
+
+    fun schedule(timer: ScheduledThreadPoolExecutor) {
+        if (invokePeriod.inWholeMilliseconds > 0) {
+            timer.scheduleAtFixedRate(this, initialDelay.inWholeMilliseconds, invokePeriod.inWholeMilliseconds, TimeUnit.MILLISECONDS)
+        }
+    }
+}
