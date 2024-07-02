794a42c271ae06a1d12eb48398cf46a581d1fe49
Benjamin Holzman <bholzman@gmail.com>
2024-07-02 10:18:33 -0400

    CHAIN-351 (2): some terraform/config tweaks for testnet; migration fix (#343)

--- backend/src/main/kotlin/co/chainring/core/model/db/migrations/V46_MarketMinMaxPrice.kt
+++ backend/src/main/kotlin/co/chainring/core/model/db/migrations/V46_MarketMinMaxPrice.kt
@@ -2,10 +2,12 @@ package co.chainring.core.model.db.migrations
 
 import co.chainring.core.db.Migration
 import co.chainring.core.model.db.GUIDTable
-import co.chainring.core.model.db.MarketEntity
 import co.chainring.core.model.db.MarketId
 import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
+import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.transactions.transaction
+import org.jetbrains.exposed.sql.update
 import java.math.BigDecimal
 import kotlin.math.min
 
@@ -13,6 +15,9 @@ import kotlin.math.min
 class V46_MarketMinMaxPrice : Migration() {
 
     object V46_MarketTable : GUIDTable<MarketId>("market", ::MarketId) {
+
+        val tickSize = decimal("tick_size", 30, 18)
+        val lastPrice = decimal("last_price", 30, 18)
         val minAllowedBidPrice = decimal("min_allowed_bid_price", 30, 18).nullable()
         val maxAllowedOfferPrice = decimal("max_allowed_offer_price", 30, 18).nullable()
     }
@@ -21,10 +26,10 @@ class V46_MarketMinMaxPrice : Migration() {
         transaction {
             SchemaUtils.createMissingTablesAndColumns(V46_MarketTable)
 
-            MarketEntity.all().forEach { marketEntity ->
+            V46_MarketTable.selectAll().forEach { marketRow ->
                 val levels = 1000
-                val lastPrice = marketEntity.lastPrice
-                val tickSize = marketEntity.tickSize
+                val lastPrice = marketRow[V46_MarketTable.lastPrice]
+                val tickSize = marketRow[V46_MarketTable.tickSize]
                 val halfTick = tickSize.setScale(tickSize.scale() + 1) / BigDecimal.valueOf(2)
 
                 val marketIx = min(levels / 2, (lastPrice - halfTick).divideToIntegralValue(tickSize).toInt())
@@ -34,8 +39,10 @@ class V46_MarketMinMaxPrice : Migration() {
 
                 // min/max prices are calculated based on last price (not initial price)
                 // need manual adjustment after deployment is required
-                marketEntity.minAllowedBidPrice = minAllowedBidPrice
-                marketEntity.maxAllowedOfferPrice = maxAllowedOfferPrice
+                V46_MarketTable.update({ V46_MarketTable.guid.eq(marketRow[V46_MarketTable.guid]) }) {
+                    it[this.minAllowedBidPrice] = minAllowedBidPrice
+                    it[this.maxAllowedOfferPrice] = maxAllowedOfferPrice
+                }
             }
 
             exec("""ALTER TABLE market ALTER COLUMN min_allowed_bid_price SET NOT NULL""")
