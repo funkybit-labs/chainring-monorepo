1c9629fe0826c12ce2a1b522a5932ff82e8fef73
Ievgen Mykolenko <imykolenko@chainring.co>
2024-07-12 13:54:24 +0200

    increase anvil's transaction-block-keeper param to 10k (#376)

--- docker/anvil/entrypoint.sh
+++ docker/anvil/entrypoint.sh
@@ -2,4 +2,4 @@
 
 : "${CHAIN_ID:=31338}"
 
-exec /root/.foundry/bin/anvil --block-time 1 --host 0.0.0.0 --port 8545 --state /data/anvil_state.json --transaction-block-keeper 500 --chain-id $CHAIN_ID
+exec /root/.foundry/bin/anvil --block-time 1 --host 0.0.0.0 --port 8545 --state /data/anvil_state.json --transaction-block-keeper 10000 --chain-id $CHAIN_ID
