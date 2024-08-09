1ca6f69e05d655a3606e650762a8cb93949c2fd7
Scott Burke <sburke@txkdata.com>
2024-08-09 18:10:43 -0500

    add cnames for funkybit and move corpweb over to new terraform state bucket
    Signed-off-by: Scott Burke <sburke@txkdata.com>

--- terraform/lightsail/main.tf
+++
@@ -1,9 +0,0 @@
-module "lightsail" {
-  source                      = "../modules/lightsail"
-  lightsail_instance_name     = var.lightsail_instance_name
-  lightsail_availability_zone = var.lightsail_availability_zone
-  lightsail_ip_address_type   = var.lightsail_ip_address_type
-  lightsail_blueprint         = var.lightsail_blueprint
-  lightsail_bundle_id         = var.lightsail_bundle_id
-  lightsail_static_ip_name    = var.lightsail_static_ip_name
-}
