f16910315237c581e4d4d7026551da256ef90858
Benjamin Holzman <bholzman@gmail.com>
2024-09-25 10:48:40 -0400

    fix unit test

--- integrationtests/src/test/kotlin/xyz/funkybit/integrationtests/exchange/PricesTest.kt
+++ integrationtests/src/test/kotlin/xyz/funkybit/integrationtests/exchange/PricesTest.kt
@@ -4,6 +4,7 @@ import org.http4k.client.WebsocketClient
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import xyz.funkybit.core.blockchain.ChainManager
 import xyz.funkybit.core.model.db.MarketId
 import xyz.funkybit.core.model.db.OHLCDuration
 import xyz.funkybit.integrationtests.testutils.AppUnderTestRunner
@@ -11,7 +12,6 @@ import xyz.funkybit.integrationtests.utils.ApiClient
 import xyz.funkybit.integrationtests.utils.assertPricesMessageReceived
 import xyz.funkybit.integrationtests.utils.blocking
 import xyz.funkybit.integrationtests.utils.subscribeToPrices
-import xyz.funkybit.tasks.blockchainClients
 
 @ExtendWith(AppUnderTestRunner::class)
 class PricesTest {
@@ -27,7 +27,7 @@ class PricesTest {
 
     private fun `test authenticated prices over websocket`(auth: String?) {
         val client = WebsocketClient.blocking(auth)
-        val chainId = blockchainClients.first().chainId
+        val chainId = ChainManager.getBlockchainClients().first().chainId
         client.subscribeToPrices(MarketId("BTC:$chainId/ETH:$chainId"), duration = OHLCDuration.P15M)
 
         client.assertPricesMessageReceived(MarketId("BTC:$chainId/ETH:$chainId"), duration = OHLCDuration.P15M) { msg ->
