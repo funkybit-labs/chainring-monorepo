e6c0b14752d4401f0a4de89ad188d12202828058
imykolenko <imykolenko@chainring.co>
2024-10-18 12:44:43 +0200

    add logging during sentry initialization

--- backend/src/main/kotlin/xyz/funkybit/core/telemetry/SentryUtils.kt
+++ backend/src/main/kotlin/xyz/funkybit/core/telemetry/SentryUtils.kt
@@ -1,15 +1,19 @@
 package xyz.funkybit.core.telemetry
 
+import io.github.oshai.kotlinlogging.KotlinLogging
 import io.sentry.Instrumenter
 import io.sentry.Sentry
 import io.sentry.SentryOptions
 import io.sentry.opentelemetry.OpenTelemetryLinkErrorEventProcessor
 
 object SentryUtils {
-    val enabled = System.getenv("SENTRY_DSN")?.let { true } ?: false
+    private val logger = KotlinLogging.logger {}
+
+    val enabled = System.getenv("SENTRY_DSN")?.isNotEmpty() ?: false
 
     fun init() {
         if (enabled) {
+            logger.debug { "Initializing Sentry" }
             Sentry.init { options: SentryOptions ->
                 options.dsn = System.getenv("SENTRY_DSN")
                 options.tracesSampleRate = System.getenv("TRACES_SAMPLE_RATE").toDoubleOrNull() ?: 1.0
@@ -17,6 +21,9 @@ object SentryUtils {
                 options.instrumenter = Instrumenter.OTEL
                 options.addEventProcessor(OpenTelemetryLinkErrorEventProcessor())
             }
+            logger.debug { "Sentry initialization complete" }
+        } else {
+            logger.debug { "Skipping Sentry initialization" }
         }
     }
 }
