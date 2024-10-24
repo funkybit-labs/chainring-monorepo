0c3c6868df5a7a2102235c60a1f752a80b4cfbd4
Ievgen Mykolenko <imykolenko@chainring.co>
2024-10-24 15:59:33 +0200

    Chain 275/handle maker batch max size (#575)
    * slice maker's batches into smaller pieces when exceeding max size
    
    * slice maker's batches into smaller pieces when exceeding max size
    
    * update logging

--- mocker/src/main/kotlin/xyz/funkybit/mocker/Main.kt
+++ mocker/src/main/kotlin/xyz/funkybit/mocker/Main.kt
@@ -122,7 +122,7 @@ fun main() {
     timer.cancel()
 }
 
-fun startMaker(market: Market, marketPriceOverride: BigDecimal?, liquidityPlacement: LiquidityPlacement, baseAssetAmount: BigDecimal, quoteAssetAmount: BigDecimal, keyPair: WalletKeyPair = WalletKeyPair.EVM.generate(), usePriceFeed: Boolean = false): Maker {
+fun startMaker(market: Market, marketPriceOverride: BigDecimal?, liquidityPlacement: LiquidityPlacement, baseAssetAmount: BigDecimal, quoteAssetAmount: BigDecimal, keyPair: WalletKeyPair = WalletKeyPair.EVM.generate(), usePriceFeed: Boolean = false, maxOrdersBatchSize: Int = 100): Maker {
     val baseAssetBtc = market.baseSymbol.value.startsWith("BTC")
     val quoteAssetBtc = market.quoteSymbol.value.startsWith("BTC")
     val baseAsset = market.baseSymbol.value to baseAssetAmount.toFundamentalUnits(market.baseDecimals)
@@ -146,7 +146,8 @@ fun startMaker(market: Market, marketPriceOverride: BigDecimal?, liquidityPlacem
             else -> mapOf(baseAsset, quoteAsset)
         },
         keyPair = keyPair,
-        usePriceFeed = usePriceFeed
+        usePriceFeed = usePriceFeed,
+        maxOrdersBatchSize = maxOrdersBatchSize
     )
     maker.start()
     return maker
